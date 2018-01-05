2f86fa796f2ab13b5a2f376dc13e9460f67d3ba5
cgosse <alec@metismachine.com>
2018-01-05 11:55:37 -0500

    calling it a new version

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,6 @@ setup(name='graphframes',
       maintainer='Metismachine',  
       maintainer_email='info@metismachine.com',
       url='http://metismachine.com',
-      version='0.5.9',
+      version='0.5.10',
       packages=['graphframes', 'graphframes.lib']
 )
